4ca23cbb44d431171b7297aaba9991d629d3c139
shivam kevat <webhunter81@gmail.com>
2025-01-09 00:46:53 +0530

    ..

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -63,7 +63,7 @@ app.use("/api/my-bookings", bookingRoutes);
 app.use("/api/payment", paymentRoutes);
 
 app.get("*", (req: Request, res: Response) => {
-  res.sendFile(path.join(process.cwd(),'client','dist','index.html'));
+  res.sendFile(path.join(process.cwd(),'frontend','dist','index.html'));
 });
 
 app.listen(7000, () => {
