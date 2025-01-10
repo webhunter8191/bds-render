967852e75b81434737e0bb36ebcb31fb4311959b
shivam kevat <webhunter81@gmail.com>
2025-01-10 17:01:43 +0530

    domain change

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -111,8 +111,9 @@ router.post(
       res.cookie("auth_token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "none",
+        sameSite: "strict",
         maxAge: 86400000, // 1 day
+        domain:"https://sparkly-brioche-d09a09.netlify.app"
       });
 
       res.status(200).json({ userId: user._id });
