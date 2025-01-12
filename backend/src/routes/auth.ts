04ee740e692aab26f36df70892ee64c433f671f0
shivam kevat <webhunter81@gmail.com>
2025-01-12 20:57:32 +0530

    domain change emv

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -113,7 +113,7 @@ router.post(
         secure: process.env.NODE_ENV === "production",
         sameSite: "none",
         maxAge: 86400000, // 1 day
-        domain:"https://bds-render-1.onrender.com"
+        domain: process.env.FRONTEND_URL
       });
 
       res.status(200).json({ userId: user._id });
