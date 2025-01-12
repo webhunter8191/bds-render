640e3e54f2e0a4abad0e5c5f8d636a21b0eb63ea
shivam kevat <webhunter81@gmail.com>
2025-01-12 15:30:21 +0530

    auth changed

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -113,7 +113,7 @@ router.post(
         secure: process.env.NODE_ENV === "production",
         sameSite: "none",
         maxAge: 86400000, // 1 day
-        // domain:"https://sparkly-brioche-d09a09.netlify.app"
+        domain:"https://bds-render-1.onrender.com"
       });
 
       res.status(200).json({ userId: user._id });
